1ba77f1adbbbe58bb4ffe959e7b1b86cc0144d7a
Edi Susanto <edsusantoo@gmail.com>
2019-04-19 17:02:24 +0700

    add string data (belum semua)

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/daftarfilm/DaftarFilmFragment.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/daftarfilm/DaftarFilmFragment.java
@@ -25,7 +25,6 @@ public class DaftarFilmFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_daftar_film, container, false);
     }
 
