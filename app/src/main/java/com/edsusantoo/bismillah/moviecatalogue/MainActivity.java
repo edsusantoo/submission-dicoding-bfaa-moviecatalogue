98d8dc793d4a43a73f0c69101c9f3a848483f50d
Edi Susanto <edsusantoo@gmail.com>
2019-04-19 16:26:47 +0700

    add library dan memunculkan daftarfilmfragment di mainactivity

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/MainActivity.java
@@ -1,7 +1,12 @@
 package com.edsusantoo.bismillah.moviecatalogue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edsusantoo.bismillah.moviecatalogue.daftarfilm.DaftarFilmFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showDaftarFilmFragment();
+
+    }
+
+    private void showDaftarFilmFragment() {
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        DaftarFilmFragment mDaftarFilmFragment = new DaftarFilmFragment();
+
+        Fragment fragment = mFragmentManager.findFragmentByTag(DaftarFilmFragment.class.getSimpleName());
+        if (!(fragment instanceof DaftarFilmFragment)) {
+            mFragmentTransaction.add(R.id.frame_container, mDaftarFilmFragment, DaftarFilmFragment.class.getSimpleName());
+            mFragmentTransaction.commit();
+        }
     }
 }
---
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/daftarfilm/DaftarFilmFragment.java
@@ -0,0 +1,36 @@
+package com.edsusantoo.bismillah.moviecatalogue.daftarfilm;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.edsusantoo.bismillah.moviecatalogue.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DaftarFilmFragment extends Fragment {
+
+
+    public DaftarFilmFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daftar_film, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
