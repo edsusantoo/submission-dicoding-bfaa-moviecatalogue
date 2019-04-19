27d7656164a92e03363c573390ed5c050c9ef18f
Edi Susanto <edsusantoo@gmail.com>
2019-04-19 19:38:07 +0700

    membuat pojo movie dan parcelable

---
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/daftarfilm/Movie.java
@@ -0,0 +1,99 @@
+package com.edsusantoo.bismillah.moviecatalogue.daftarfilm;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+    private int photo;
+    private String title;
+    private String date;
+    private String description;
+    private String rate;
+    private String revenue;
+
+    public Movie() {
+    }
+
+    private Movie(Parcel in) {
+        this.photo = in.readInt();
+        this.title = in.readString();
+        this.date = in.readString();
+        this.description = in.readString();
+        this.rate = in.readString();
+        this.revenue = in.readString();
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(String revenue) {
+        this.revenue = revenue;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.photo);
+        dest.writeString(this.title);
+        dest.writeString(this.date);
+        dest.writeString(this.description);
+        dest.writeString(this.rate);
+        dest.writeString(this.revenue);
+    }
+}
