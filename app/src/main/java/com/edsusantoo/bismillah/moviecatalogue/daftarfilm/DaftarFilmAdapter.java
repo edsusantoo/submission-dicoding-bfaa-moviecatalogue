e139729fc27745754b07cb23d1a3b55bbf5ad6a3
Edi Susanto <edsusantoo@gmail.com>
2019-04-19 19:44:28 +0700

    membuat adapter daftarfilm

---
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/daftarfilm/DaftarFilmAdapter.java
@@ -0,0 +1,70 @@
+package com.edsusantoo.bismillah.moviecatalogue.daftarfilm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.edsusantoo.bismillah.moviecatalogue.R;
+
+import java.util.ArrayList;
+
+public class DaftarFilmAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Movie> movies;
+
+    public DaftarFilmAdapter(Context context, ArrayList<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @Override
+    public int getCount() {
+        return movies.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return movies.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_movie, viewGroup, false);
+        }
+        ViewHolder viewHolder = new ViewHolder(view);
+        Movie movie = (Movie) getItem(position);
+        viewHolder.bind(movie);
+
+        return view;
+    }
+
+    private class ViewHolder {
+        private TextView tvTittle, tvDate, tvDescription;
+        private ImageView imgMovie;
+
+        ViewHolder(View view) {
+            tvTittle = view.findViewById(R.id.tv_title);
+            tvDate = view.findViewById(R.id.tv_date);
+            tvDescription = view.findViewById(R.id.tv_description);
+            imgMovie = view.findViewById(R.id.img_movie);
+        }
+
+        void bind(Movie movie) {
+            tvTittle.setText(movie.getTitle());
+            tvDate.setText(movie.getDate());
+            tvDescription.setText(movie.getDescription());
+            imgMovie.setImageResource(movie.getPhoto());
+        }
+    }
+}
