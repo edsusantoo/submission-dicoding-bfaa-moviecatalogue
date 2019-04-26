350420cf2349ac12efcfd0828253b9dd4b1d7dd0
Edi Susanto <edsusantoo@gmail.com>
2019-04-26 15:08:38 +0700

    implementasi change language

---
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/changelanguage/ChangeLanguage.java
@@ -0,0 +1,77 @@
+package com.edsusantoo.bismillah.moviecatalogue.ui.changelanguage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.edsusantoo.bismillah.moviecatalogue.R;
+
+public class ChangeLanguage extends AppCompatActivity {
+    public static final String EXTRA_LANGUAGE = "extra_language";
+    private String language = null;
+    private RadioGroup rgChangeLanguage;
+    private RadioButton rbIndonesia, rbEnglish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_language);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        rgChangeLanguage = findViewById(R.id.rg_change_language);
+        rbIndonesia = findViewById(R.id.rb_indonesia);
+        rbEnglish = findViewById(R.id.rb_english);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        setRadiButtonLanguage();
+
+    }
+
+    private void setRadiButtonLanguage() {
+        language = getIntent().getStringExtra(EXTRA_LANGUAGE);
+        if (language != null) {
+            switch (language) {
+                case "in":
+                    rbIndonesia.setChecked(true);
+                    break;
+                case "en":
+                    rbEnglish.setChecked(true);
+                    break;
+            }
+        }
+    }
+
+    private String getRadioChangeLanguage() {
+        int checkedRadioButtonId = rgChangeLanguage.getCheckedRadioButtonId();
+        language = null;
+        if (checkedRadioButtonId != -1) {
+            switch (checkedRadioButtonId) {
+                case R.id.rb_indonesia:
+                    language = "in";
+                    break;
+                case R.id.rb_english:
+                    language = "en";
+                    break;
+            }
+        }
+        return language;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent();
+        i.putExtra(EXTRA_LANGUAGE, getRadioChangeLanguage());
+        setResult(RESULT_OK, i);
+        super.onBackPressed();
+    }
+}
--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/main/MainActivity.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/main/MainActivity.java
@@ -1,14 +1,24 @@
 package com.edsusantoo.bismillah.moviecatalogue.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.edsusantoo.bismillah.moviecatalogue.R;
+import com.edsusantoo.bismillah.moviecatalogue.ui.changelanguage.ChangeLanguage;
 import com.edsusantoo.bismillah.moviecatalogue.ui.main.adapter.MainViewPagerAdapater;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int REQUEST_CODE_CHANGE_LANGUAGE = 101;
+    private String language = null;
 
     private TabLayout tabLayoutMain;
     private ViewPager viewPagerMain;
@@ -18,11 +28,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imgSetting = findViewById(R.id.img_setting);
         tabLayoutMain = findViewById(R.id.tab_main);
         viewPagerMain = findViewById(R.id.view_pager_main);
 
         setViewPagerMain();
         setTabLayoutMain();
+
+        imgSetting.setOnClickListener(this);
 
 
     }
@@ -34,6 +47,46 @@ public class MainActivity extends AppCompatActivity {
 
     private void setTabLayoutMain() {
         tabLayoutMain.setupWithViewPager(viewPagerMain);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img_setting:
+                Intent i = new Intent(MainActivity.this, ChangeLanguage.class);
+                i.putExtra(ChangeLanguage.EXTRA_LANGUAGE, language);
+                startActivityForResult(i, REQUEST_CODE_CHANGE_LANGUAGE);
+                break;
+        }
+    }
+
+
+    private void setLanguage(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHANGE_LANGUAGE) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    language = data.getStringExtra(ChangeLanguage.EXTRA_LANGUAGE);
+                    if (language != null) {
+                        setLanguage(language);
+
+                        //untuk refresh
+                        setTabLayoutMain();
+                        setViewPagerMain();
+                    }
+                }
+            }
+        }
     }
 
 }
