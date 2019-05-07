65860ac94c1d270cfa2a6e2b47a8c8cf22551fb8
Edi Susanto <edsusantoo@gmail.com>
2019-05-07 10:05:15 +0700

    add api service movie dan tvshow

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/network/ApiServices.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/network/ApiServices.java
@@ -1,5 +1,22 @@
 package com.edsusantoo.bismillah.moviecatalogue.data.network;
 
-public interface ApiServices {
+import com.edsusantoo.bismillah.moviecatalogue.data.network.model.movie.MovieResponse;
+import com.edsusantoo.bismillah.moviecatalogue.data.network.model.tvshow.TvShowResponse;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiServices {
+    @GET("movie")
+    Call<MovieResponse> getMovie(
+            @Query("api_key") String api_key,
+            @Query("language") String language
+    );
+
+    @GET("tv")
+    Call<TvShowResponse> getTvMovie(
+            @Query("api_key") String api_key,
+            @Query("language") String language
+    );
 }
