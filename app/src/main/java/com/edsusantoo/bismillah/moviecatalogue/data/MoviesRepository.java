589b6140743e9eb1ec319b077546f7cd87d6d0fe
Edi Susanto <edsusantoo@gmail.com>
2019-06-16 08:17:54 +0700

    konfigurasi repository

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/MoviesRepository.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/MoviesRepository.java
@@ -1,13 +1,23 @@
 package com.edsusantoo.bismillah.moviecatalogue.data;
 
+import android.arch.lifecycle.LiveData;
 import android.content.Context;
 
+import com.edsusantoo.bismillah.moviecatalogue.data.db.RootMoviesDB;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.dao.FavoriteDao;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.dao.MovieDao;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.dao.UserDao;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.Favorites;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.Movie;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.User;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.ApiServices;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.RetrofitConfig;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.model.movie.MovieResponse;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.model.tvshow.TvShowResponse;
 import com.edsusantoo.bismillah.moviecatalogue.data.pref.SharedPref;
 import com.edsusantoo.bismillah.moviecatalogue.utils.Constant;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.disposables.CompositeDisposable;
@@ -16,11 +26,28 @@ public class MoviesRepository implements Repository {
     private ApiServices apiServices;
     private CompositeDisposable compositeDisposable;
     private SharedPref pref;
+    private MovieDao movieDao;
+    private UserDao userDao;
+    private FavoriteDao favoriteDao;
+
+    private LiveData<List<Movie>> allMovies;
+    private LiveData<List<Favorites>> allFavorites;
+    private LiveData<List<User>> allUsers;
 
     public MoviesRepository(Context context) {
         apiServices = RetrofitConfig.getApiServices();
         compositeDisposable = new CompositeDisposable();
         pref = new SharedPref(context);
+        RootMoviesDB rootMoviesDB = RootMoviesDB.getDatabase(context);
+
+        //RoomDao
+        movieDao = rootMoviesDB.movieDao();
+        userDao = rootMoviesDB.userDao();
+        favoriteDao = rootMoviesDB.favoriteDao();
+
+        allMovies = movieDao.getAll();
+
+
     }
 
 
@@ -32,6 +59,36 @@ public class MoviesRepository implements Repository {
     @Override
     public Observable<TvShowResponse> getTvShow(String api_key, String language) {
         return apiServices.getTvMovie(api_key, language);
+    }
+
+    @Override
+    public void insertUser(User user) {
+        userDao.insert(user);
+    }
+
+    @Override
+    public void insertMovie(Movie movie) {
+        movieDao.insert(movie);
+    }
+
+    @Override
+    public void insertFavorite(Favorites favorites) {
+        favoriteDao.insert(favorites);
+    }
+
+    @Override
+    public LiveData<List<Movie>> getAllMovie() {
+        return allMovies;
+    }
+
+    @Override
+    public LiveData<List<User>> getAllUser() {
+        return allUsers;
+    }
+
+    @Override
+    public LiveData<List<Favorites>> getAllFavorites() {
+        return allFavorites;
     }
 
     @Override
--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/Repository.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/data/Repository.java
@@ -1,7 +1,14 @@
 package com.edsusantoo.bismillah.moviecatalogue.data;
 
+import android.arch.lifecycle.LiveData;
+
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.Favorites;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.Movie;
+import com.edsusantoo.bismillah.moviecatalogue.data.db.model.User;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.model.movie.MovieResponse;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.model.tvshow.TvShowResponse;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 
@@ -9,6 +16,18 @@ interface Repository {
     Observable<MovieResponse> getMovie(String api_key, String language);
 
     Observable<TvShowResponse> getTvShow(String api_key, String language);
+
+    void insertUser(User user);
+
+    void insertMovie(Movie movie);
+
+    void insertFavorite(Favorites favorites);
+
+    LiveData<List<Movie>> getAllMovie();
+
+    LiveData<List<User>> getAllUser();
+
+    LiveData<List<Favorites>> getAllFavorites();
 
     String getLanguage();
 
