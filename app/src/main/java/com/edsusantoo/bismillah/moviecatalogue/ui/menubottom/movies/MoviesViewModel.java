59bce8330c9bd81198e0754cd5267a948ec59e54
Edi Susanto <edsusantoo@gmail.com>
2019-06-17 10:33:38 +0700

    delete method insert dan delete movie favorite

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/menubottom/movies/MoviesViewModel.java
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/menubottom/movies/MoviesViewModel.java
@@ -7,15 +7,10 @@ import android.arch.lifecycle.MutableLiveData;
 
 import com.edsusantoo.bismillah.moviecatalogue.BuildConfig;
 import com.edsusantoo.bismillah.moviecatalogue.data.MoviesRepository;
-import com.edsusantoo.bismillah.moviecatalogue.data.db.model.Favorites;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.ApiObserver;
 import com.edsusantoo.bismillah.moviecatalogue.data.network.model.movie.MovieResponse;
 
-import io.reactivex.Completable;
-import io.reactivex.CompletableObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
 import io.reactivex.schedulers.Schedulers;
 
 public class MoviesViewModel extends AndroidViewModel {
@@ -85,110 +80,6 @@ public class MoviesViewModel extends AndroidViewModel {
                     public void onFailure(String message) {
                         isLoading.setValue(false);
                         dataMovies.setValue(null);
-                    }
-                });
-    }
-
-    void insertMovie(final com.edsusantoo.bismillah.moviecatalogue.data.db.model.Movie movie) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                repository.insertMovie(movie);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new CompletableObserver() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        messageSuccess.setValue("Successful add movie");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        messageError.setValue(e.getMessage());
-                    }
-                });
-    }
-
-    void insertFavorite(final Favorites favorites) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                repository.insertFavorite(favorites);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new CompletableObserver() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        messageSuccess.setValue("Successful add favorite");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        messageError.setValue(e.getMessage());
-                    }
-                });
-    }
-
-    void deleteMovie(final com.edsusantoo.bismillah.moviecatalogue.data.db.model.Movie movie) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                repository.deleteMovie(movie);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new CompletableObserver() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        messageSuccess.setValue("Movie Berhasil Dihapus");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        messageError.setValue(e.getMessage());
-                    }
-                });
-    }
-
-    void deleteFavorite(final Favorites favorites) {
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                repository.deleteFavorite(favorites);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new CompletableObserver() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        messageSuccess.setValue("Favorite Berhasil Dihapus");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        messageError.setValue(e.getMessage());
                     }
                 });
     }
